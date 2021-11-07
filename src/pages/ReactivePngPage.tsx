068f9c4172ad3b4470001f733f207d148fcfdc64
Malooski <malooski@protonmail.com>
2021-11-06 19:55:46 -0500

    Fix more warnings.

--- src/pages/ReactivePngPage.tsx
+++ src/pages/ReactivePngPage.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useRef, useState } from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import ColumnDiv from "../components/ColDiv";
 import CopyArea from "../components/CopyArea";
 
